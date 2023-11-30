f5094f46cd628c70b95a268168fa543d699a1b12
Michael Mace <michael.mace@conexon.us>
2023-11-29 21:28:12 -0600

    vercel cli working local server

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = require('express')()
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end('Hello server!')
+  res.end(<div>hello API!</div>)
 })
 
 module.exports = app
