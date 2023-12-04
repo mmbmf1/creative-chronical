c2f8c24e1c195972cfa53bf6b8e0e8c6532046f5
Michael Mace <michael.mace@conexon.us>
2023-12-04 09:43:31 -0600

    init commit; mock up the about endpoint to use some htmx

--- api/about/index.js
+++ api/about/index.js
@@ -3,7 +3,7 @@ const app = require('express')()
 app.get('/api/about', (req, res) => {
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end('Hello About API')
+  res.send('<h1>Hello About API</h1>')
 })
 
 module.exports = app
