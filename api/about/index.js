c17a93d9800f1fe14508550b707ced75856b137a
Michael Mace <michael.mace@conexon.us>
2023-12-05 23:01:20 -0600

    play with pug htmx and express

--- api/about/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')()
-
-app.get('/api/about', (req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.send('<h1>Hello About API</h1>')
-})
-
-module.exports = app
--- api/index.js
+++ api/index.js
@@ -1,9 +1,32 @@
-const app = require('express')()
+const express = require('express')
+const pug = require('pug')
+
+const app = express()
+
+app.set('view engine', 'pug')
 
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end('Hello API')
+  res.render('index')
+})
+
+app.get('/api/about', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.render('about')
+})
+
+app.get('/api/projects', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.render('projects')
+})
+
+app.get('/api/contact', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.render('contact')
 })
 
 module.exports = app
