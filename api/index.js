a88ad3c8f97770d0570503eff5bd017a6558cee9
Michael Mace <michael.mace@conexon.us>
2023-12-07 13:44:02 -0600

    connect api to data and template

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const pug = require('pug')
+import projects from '../assets/project_list'
 
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'pug')
@@ -18,9 +20,14 @@ app.get('/api/about', (req, res) => {
 })
 
 app.get('/api/projects', (req, res) => {
+  console.log('projects', projects)
+  console.log('match on a project id')
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.render('projects')
+
+  const project_list = pug.compileFile('views/projects.pug')
+
+  res.send(project_list(projects[0]))
 })
 
 app.get('/api/contact', (req, res) => {
---
+++ assets/project_list.js
@@ -0,0 +1,9 @@
+const projects = [
+  {
+    id: 1,
+    name: 'first test project',
+    image_url: 'https://avatars.githubusercontent.com/u/53282698?v=4',
+  },
+]
+
+export default projects
