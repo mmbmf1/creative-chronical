821ebd02f32e4f598f540a10e27bc1b5cafc9057
Michael Mace <michael.mace@conexon.us>
2023-12-07 18:55:34 -0600

    mock data clean up

--- assets/project_list.js
+++ assets/project_list.js
@@ -1,18 +1,23 @@
 const projects = [
   {
     id: 1,
-    name: 'first test project',
+    name: 'first project',
     image_url: 'https://picsum.photos/id/13/625/415',
   },
   {
     id: 2,
-    name: 'second test project',
+    name: 'second project',
     image_url: 'https://picsum.photos/id/17/625/415',
   },
   {
     id: 3,
-    name: 'third test project',
+    name: 'third project',
     image_url: 'https://picsum.photos/id/22/1108/933',
+  },
+  {
+    id: 4,
+    name: 'n project',
+    image_url: 'https://picsum.photos/id/11/1108/933',
   },
 ]
 
