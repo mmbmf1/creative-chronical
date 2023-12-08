d4b40b0c443b1b6f349248ba67d69b274ad48388
Michael Mace <michael.mace@conexon.us>
2023-12-07 23:46:34 -0600

    more mock content

--- assets/details.js
+++ assets/details.js
@@ -1,22 +1,26 @@
 const details = [
   {
     id: 1,
-    summary: 'Lorem ipsum.',
+    summary:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
     id: 2,
-    summary: 'Lorem ipsum.',
+    summary:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
     id: 3,
-    summary: 'Lorem ipsum.',
+    summary:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
     id: 4,
-    summary: 'Lorem ipsum.',
+    summary:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
 ]
