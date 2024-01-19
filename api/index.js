0199fb0176c94fb897332e0694cdc2d50529724c
Michael Mace <michael.mace@conexon.us>
2024-01-19 10:51:11 -0600

    add try catch

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
 
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'pug')
@@ -35,82 +34,103 @@ app.get('/api/about', (req, res) => {
 app.get('/api/projects', async (req, res) => {
   const { page } = req.query
 
-  let { data: projects, error: projects_error } = await supabase
-    .from('projects')
-    .select('*')
+  const supabase = createClient(supabaseUrl, supabaseKey)
 
-  if (projects_error) throw new Error(projects_error)
+  try {
+    let { data: projects, error: projects_error } = await supabase
+      .from('projects')
+      .select('*')
 
-  const itemsPerPage = 6
-  const startIndex = (page - 1) * itemsPerPage
-  const endIndex = page * itemsPerPage
-  const pagedProjects = projects.slice(startIndex, endIndex)
+    if (projects_error) throw new Error(projects_error)
 
-  const totalPages = Math.ceil(projects.length / itemsPerPage)
-  const paginationLinks = Array.from({ length: totalPages }, (_, i) => i + 1)
+    const itemsPerPage = 6
+    const startIndex = (page - 1) * itemsPerPage
+    const endIndex = page * itemsPerPage
+    const pagedProjects = projects.slice(startIndex, endIndex)
 
-  const projectsArray = pug.compileFile('views/projects.pug')
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.send(
-    projectsArray({
-      projects: pagedProjects,
-      pagination: paginationLinks,
-      current_page: page,
-    })
-  )
+    const totalPages = Math.ceil(projects.length / itemsPerPage)
+    const paginationLinks = Array.from({ length: totalPages }, (_, i) => i + 1)
+
+    const projectsArray = pug.compileFile('views/projects.pug')
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+    res.send(
+      projectsArray({
+        projects: pagedProjects,
+        pagination: paginationLinks,
+        current_page: page,
+      })
+    )
+  } catch (error) {
+    console.log('👀 🔍 ~ app.get ~ error:', error)
+    throw new Error(error)
+  }
 })
 
 app.get('/api/details', async (req, res) => {
   const { id, page, image } = req.query
 
-  let { data: details, error: details_error } = await supabase
-    .from('details')
-    .select('*')
-    .eq('id', id)
+  const supabase = createClient(supabaseUrl, supabaseKey)
 
-  if (details_error) throw new Error(details_error)
+  try {
+    let { data: details, error: details_error } = await supabase
+      .from('details')
+      .select('*')
+      .eq('id', id)
 
-  let { data: projects, error: projects_error } = await supabase
-    .from('projects')
-    .select('name, image_url')
-    .eq('id', id)
+    if (details_error) throw new Error(details_error)
 
-  if (projects_error) throw new Error(projects_error)
+    let { data: projects, error: projects_error } = await supabase
+      .from('projects')
+      .select('name, image_url')
+      .eq('id', id)
 
-  if (image) {
-    const image = pug.compileFile('views/image.pug')
+    if (projects_error) throw new Error(projects_error)
+
+    if (image) {
+      const image = pug.compileFile('views/image.pug')
+      res.setHeader('Content-Type', 'text/html')
+      res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+      res.send(image(projects[0].image_url))
+    }
+
+    details[0].name = projects[0].name
+    details[0].image_url = projects[0].image_url
+    details[0].current_page = page
+
     res.setHeader('Content-Type', 'text/html')
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-    res.send(image(projects[0].image_url))
+    const project = pug.compileFile('views/details.pug')
+    res.send(project(details[0]))
+  } catch (error) {
+    console.log('👀 🔍 ~ app.get ~ error:', error)
+    throw new Error(error)
   }
-
-  details[0].name = projects[0].name
-  details[0].image_url = projects[0].image_url
-  details[0].current_page = page
-
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  const project = pug.compileFile('views/details.pug')
-  res.send(project(details[0]))
 })
 
 app.get('/api/image', async (req, res) => {
   const { id, page } = req.query
 
-  let { data: projects, error: projects_error } = await supabase
-    .from('projects')
-    .select('*')
-    .eq('id', id)
+  const supabase = createClient(supabaseUrl, supabaseKey)
 
-  if (projects_error) throw new Error(projects_error)
+  try {
+    let { data: projects, error: projects_error } = await supabase
+      .from('projects')
+      .select('*')
+      .eq('id', id)
 
-  const { image_url } = projects[0]
+    if (projects_error) throw new Error(projects_error)
 
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  const image = pug.compileFile('views/image.pug')
-  res.send(image({ image_url, id, current_page: page }))
+    const { image_url } = projects[0]
+
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+    const image = pug.compileFile('views/image.pug')
+    res.send(image({ image_url, id, current_page: page }))
+  } catch (error) {
+    console.log('👀 🔍 ~ app.get ~ error:', error)
+    throw new Error(error)
+  }
 })
 
 app.get('/api/contact', (req, res) => {
