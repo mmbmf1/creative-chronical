251a89cabd44048d13006b0e84a09513cfa4bb2d
Michael Mace <michael.mace@conexon.us>
2024-01-19 08:44:18 -0600

    init commit; use project id in details; prep for supabase

--- assets/details.js
+++ assets/details.js
@@ -1,90 +1,90 @@
 const details = [
   {
-    id: 1,
+    project_id: 1,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 2,
+    project_id: 2,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 3,
+    project_id: 3,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 4,
+    project_id: 4,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 5,
+    project_id: 5,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 6,
+    project_id: 6,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 7,
+    project_id: 7,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 8,
+    project_id: 8,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 9,
+    project_id: 9,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 10,
+    project_id: 10,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 11,
+    project_id: 11,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 12,
+    project_id: 12,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 13,
+    project_id: 13,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 14,
+    project_id: 14,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
   },
   {
-    id: 15,
+    project_id: 15,
     summary:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend pellentesque lacus, non dignissim turpis rutrum et. Sed id pharetra ligula, et volutpat mauris. Ut sit amet massa non urna cursus posuere. Curabitur venenatis ultrices tortor, id congue magna pharetra ac. Curabitur a efficitur tellus. Nullam sit amet aliquet est.',
     tech: ['JS', 'NextJS', 'TailwindCSS'],
